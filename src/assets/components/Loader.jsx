b037c8b50051b1fc946fa2de5ce048e9e53336ca
sameer4712 <sameerptmc@gmail.com>
2025-12-04 15:33:16 +0530

    centerd the snake

--- src/assets/components/Loader.jsx
+++ src/assets/components/Loader.jsx
@@ -1,10 +1,10 @@
 export default function Loader() {
   return (
     <div className="fixed inset-0 bg-black flex flex-col justify-center items-center z-50">
-      <div className="snake-loader flex justify-center items-center">
+      <div className="snake-loader md:ml-14 ml-10 ">
         <span></span><span></span><span></span><span></span>
       </div>
-      <p className="text-amber-300 font-mono text-lg mt-6 ml-14 animate-pulse">
+      <p className="text-amber-300 font-mono text-lg mt-4 md:ml-14 ml-7 animate-pulse">
         Portfolio is loading...
       </p>
 
