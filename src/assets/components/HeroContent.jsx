f84b2da19849295eec038e849cb84c90ede8d7f3
sameer4712 <sameerptmc@gmail.com>
2026-03-13 16:16:08 +0530

    pdf changed

--- src/assets/components/HeroContent.jsx
+++ src/assets/components/HeroContent.jsx
@@ -24,7 +24,7 @@ export default function HeroContent() {
       <div className='flex gap-6 mt-4'>
         <a
         href='/sameer.pdf'
-          download="/resume.pdf"
+          download="/Sameer-FullStack-Developer.pdf"
           rel="noopener noreferrer"
         >
           <button className="font-mono border border-amber-300 px-4 py-2 rounded-2xl hover:bg-amber-300 hover:text-black transition cursor-pointer">
